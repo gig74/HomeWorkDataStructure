25a5cf49f51abe740a6208695202d584bbd7163a
gig74 <gig74_tmp@mail.ru>
2024-04-22 12:01:50 +0500

    03 - my test 1

--- src/Solution2.java
+++ src/Solution2.java
@@ -2,29 +2,29 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class Solution2 {
-    private static final String OPEN_STAPLES = "([{" ;
-    private static final String CLOSE_STAPLES = ")]}" ;
+    private static final String OPEN_STAPLES = "([{";
+    private static final String CLOSE_STAPLES = ")]}";
+
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for ( char ch : s.toCharArray()) {
-          if (OPEN_STAPLES.indexOf(ch) >= 0){
-              stack.push(ch);
-          }
-          else if(CLOSE_STAPLES.indexOf(ch) >= 0) {
-              if (stack.empty()) {
-                  return false;
-              }
-              char currentOpenStaple = (char) stack.pop();
-              if (OPEN_STAPLES.indexOf(currentOpenStaple) != CLOSE_STAPLES.indexOf(ch)){
-                  return false;
-              }
-          }
-          else {
-              throw new RuntimeException("I see illegal symbol");
-          }
+        for (char ch : s.toCharArray()) {
+            if (OPEN_STAPLES.indexOf(ch) >= 0) {
+                stack.push(ch);
+            } else if (CLOSE_STAPLES.indexOf(ch) >= 0) {
+                if (stack.empty()) {
+                    return false;
+                }
+                char currentOpenStaple = (char) stack.pop();
+                if (OPEN_STAPLES.indexOf(currentOpenStaple) != CLOSE_STAPLES.indexOf(ch)) {
+                    return false;
+                }
+            } else {
+                throw new RuntimeException("I see illegal symbol");
+            }
         }
         return stack.isEmpty();
     }
+
     public static void test() {
         System.out.println(isValid("()"));
         System.out.println(isValid("()[]{}"));
@@ -32,6 +32,10 @@ public class Solution2 {
         System.out.println(isValid("(()"));
 
     }
+
+    //    public static void main(String[] args) {
+//        Solution2.test();
+//    }
     public static void main(String[] args) {
         Solution2.test();
     }
